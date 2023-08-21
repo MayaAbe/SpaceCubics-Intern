909f64e84e62d5d77a55bffc9a3a46762116109d
ABema <a.maya3141592@icloud.com>
2023-08-21 17:30:52 +0900

    Network Fix: GlobalVar:ALLCAPS

--- Network/udp_RP.py
+++ Network/udp_RP.py
@@ -12,37 +12,37 @@ def main():
     SOCK.bind(("192.168.3.5", UDP_PORT))
 
     while True:
-        data, addr = SOCK.recvfrom(BUFFER_SIZE)
-        print(f"Received message: {data} from {addr}")
+        DATA, ADDR = SOCK.recvfrom(BUFFER_SIZE)
+        print(f"Received message: {DATA} from {ADDR}")
 
-        if data == b"a":
+        if DATA == b"a":
             print("Camera activate")
-            SOCK.sendto(b"0", addr)
-            SOCK.sendto(b"A", addr)  # Return byte data 'A' to the PC
+            SOCK.sendto(b"0", ADDR)
+            SOCK.sendto(b"A", ADDR)  # Return byte data 'A' to the PC
 
             response, _ = SOCK.recvfrom(1)  # Wait for a reply from the PC
             if response == b"j":
                 subprocess.run(["libcamera-jpeg", "-o", "test.jpg"], check=True)
-                SOCK.sendto(b"j", addr)  # Return byte data 'j' to the PC
+                SOCK.sendto(b"j", ADDR)  # Return byte data 'j' to the PC
             if response == b"r":
                 subprocess.run(
                     ["libcamera-raw", "-t", "2000", "-o", "test.raw"], check=True
                 )
-                SOCK.sendto(b"r", addr)  # Return byte data 'r' to the PC
+                SOCK.sendto(b"r", ADDR)  # Return byte data 'r' to the PC
             if response == b"n":
-                SOCK.sendto(b"n", addr)  # Return byte data 'n' to the PC
-        elif data == b"b":
+                SOCK.sendto(b"n", ADDR)  # Return byte data 'n' to the PC
+        elif DATA == b"b":
             print("HK data are ...")
             response = "HK data abcdef...".encode("utf-8")
-            SOCK.sendto(response, addr)
-        elif data == b"c":
+            SOCK.sendto(response, ADDR)
+        elif DATA == b"c":
             print("reboot")
             response = "now rebooting".encode("utf-8")
-            SOCK.sendto(response, addr)
+            SOCK.sendto(response, ADDR)
         else:
             response = "Received an irregular signal".encode("utf-8")
-            SOCK.sendto(response, addr)
-        data = b""
+            SOCK.sendto(response, ADDR)
+        DATA = b""
 
 if __name__ == "__main__":
     main()
