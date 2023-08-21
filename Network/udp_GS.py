bb5a7f3abeb17dc30dfff4c20b42a1b24a36505f
ABema <a.maya3141592@icloud.com>
2023-08-21 16:03:37 +0900

    Network Fix: format of GS.py

--- Network/udp_GS.py
+++ Network/udp_GS.py
@@ -4,22 +4,19 @@ import time
 
 fmt_line = "----"
 
-
 # Specify IP address and UDP port number for Raspberry Pi Zero
 TARGET_IP = input("Target IP is: ")  # "192.168.3.5"
-
 UDP_PORT = 50000
 
+flag = 0
 
-# 受信機能を持つ関数
+
+# Function with receiving capability
 def receive_messages(sock):
     while True:
-
         data, addr = sock.recvfrom(1024)  # can receive up to 1024 bytes
         # print(f"----\nReceived response: {data.decode('utf-8')} \nfrom {addr}\n----")
         # print(f"{fmt_line}Response from: {addr} {fmt_line}")
-
-
         if data == b"0":
             pass
         if data == b"1":
@@ -27,7 +24,7 @@ def receive_messages(sock):
         if data == b"2":
             print("now rebooting")
         if data == b"3":
-            print("正規の信号ではありませんでした")
+            print("Received an irregular signal")
 
         if data == b"A":
             while True:
@@ -39,28 +36,25 @@ def receive_messages(sock):
 
                     if choice in ["j", "r", "n"]:
                         sock.sendto(choice.encode("utf-8"), (TARGET_IP, UDP_PORT))
-
                         break
-                print("responseを待っています")
+                print("Waiting for response")
 
                 response, _ = sock.recvfrom(1)
-
                 if response == b"j":
                     print("\n A jpeg image was taken")
-
                     break
                 if response == b"r":
-                    print("\n raw画像が撮影されました")
+                    print("\n A raw image was taken")
                     break
                 if response == b"n":
-                    print("\n 撮影を中止しました")
+                    print("\n Photo capture was cancelled")
 
 
-# ソケットの作成
+# Create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", UDP_PORT))  # 任意のアドレスからの受信を許可
+sock.bind(("0.0.0.0", UDP_PORT))  # allow reception from any address
 
-# 受信機能を別のスレッドで開始
+# Start receiving functionality in a separate thread
 rxthread = threading.Thread(target=receive_messages, args=(sock,))
 rxthread.daemon = True
 rxthread.start()
@@ -68,7 +62,7 @@ rxthread.start()
 while True:
     time.sleep(0.5)
     if flag == 0:
-        cmd = input("送信するデータをA~Cで入力してください: ")
+        cmd = input("Please enter the data to send from A to C: ")
         try:
             cmd = cmd.encode("utf-8")
         except:
