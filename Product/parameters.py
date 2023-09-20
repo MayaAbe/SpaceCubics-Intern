1c3346fdc5571c427fb0d1d1ef95e20dd3bf258c
ABema <a.maya3141592@icloud.com>
2023-09-20 17:22:41 +0900

    zero Fix: metadata list

--- Product/parameters.py
+++ Product/parameters.py
@@ -1,3 +1,38 @@
+# metadata list
+ScalerCrop = (160, 0, 960, 720)
+SensorBlackLevels = (2048, 2048, 2048, 2048)
+ColourTemperature = 4500
+DigitalGain = 1.0
+ColourGains = (1.1795026063919067, 1.0574291944503784)
+AeLocked = False
+Lux = 7597.48828125
+FrameDuration = 8330
+ColourCorrectionMatrix = (1.7592334747314453, -0.38649335503578186, -0.37275010347366333,
+                          -0.44978946447372437, 1.555491328239441, -0.10570189356803894,
+                          -0.1007557362318039, -0.46043145656585693, 1.5611871480941772)
+SensorTimestamp = 7401520219000
+AnalogueGain = 1.0
+FocusFoM = 4766
+ExposureTime = 8000
+
+
+metadata = {
+    'ScalerCrop': ScalerCrop,
+    'SensorBlackLevels': SensorBlackLevels,
+    'ColourTemperature': ColourTemperature,
+    'DigitalGain': DigitalGain,
+    'ColourGains': ColourGains,
+    'AeLocked': AeLocked,
+    'Lux': Lux,
+    'FrameDuration': FrameDuration,
+    'ColourCorrectionMatrix': ColourCorrectionMatrix,
+    'SensorTimestamp': SensorTimestamp,
+    'AnalogueGain': AnalogueGain,
+    'FocusFoM': FocusFoM,
+    'ExposureTime': ExposureTime
+}
+
+
 def check_metadata(metadata: dict):
     print("-------------------------------")
     for key, value in metadata.items():
@@ -35,19 +70,12 @@ def update_metadata(metadata: dict, key, new_value, file_path="parameters.py"):
         print(f"Key {key} not found in metadata")
 
 
-metadata = {
-        'ScalerCrop': (160, 0, 960, 720),
-        'SensorBlackLevels': (2048, 2048, 2048, 2048),
-        'ColourTemperature': 4500,
-        'DigitalGain': 1.0,
-        'ColourGains': (1.1795026063919067, 1.0574291944503784),
-        'AeLocked': False,
-        'Lux': 7597.48828125,
-        'FrameDuration': 8330,
-        'ColourCorrectionMatrix': (1.7592334747314453, -0.38649335503578186, -0.37275010347366333,
-                                   -0.44978946447372437, 1.555491328239441, -0.10570189356803894,
-                                   -0.1007557362318039, -0.46043145656585693, 1.5611871480941772),
-        'SensorTimestamp': 7401520219000,
-        'AnalogueGain': 1.0,
-        'FocusFoM': 4766,
-        'ExposureTime': 8000}
+def describe_metadata(metadata):
+    description = {}
+    for key, value in metadata.items():
+        description[key] = {'value': value, 'type': type(value).__name__}
+        print(f"{key}:\n  Value: {value}\n  Type: {type(value).__name__}\n")
+
+
+if __name__ == "__main__":
+    print(describe_metadata(metadata))
