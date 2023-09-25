8877247a3235338db7d224bced169793c562359f
ABema <a.maya3141592@icloud.com>
2023-09-25 15:03:28 +0900

    update capture
    dngとjpgを同時撮影する関数の引数を統一して同じ名前で
    拡張子が異なるファイルが保存されるようにした
    
    Signed-off-by: ABema <a.maya3141592@icloud.com>

--- Product/capture.py
+++ Product/capture.py
@@ -6,7 +6,7 @@ from picamera2 import Picamera2
 import parameters as param  # Assuming parameters.py contains common parameters
 
 
-def capture_dng_jpeg(output_filename_dng="capture.dng", output_filename_jpeg="capture.jpg"):
+def capture_dng_jpeg(output_filename="image0"):
     picam2 = Picamera2()
 
     exposure = param.metadata["ExposureTime"]  # Read from parameters module
@@ -19,8 +19,8 @@ def capture_dng_jpeg(output_filename_dng="capture.dng", output_filename_jpeg="ca
     time.sleep(2)
 
     r = picam2.switch_mode_capture_request_and_stop(capture_config)
-    r.save("main", output_filename_jpeg)
-    r.save_dng(output_filename_dng)
+    r.save("main", output_filename+".jpg")
+    r.save_dng(output_filename+".dng")
 
     picam2.stop()
 
@@ -45,7 +45,7 @@ def capture_dng(output_filename="cap_dng.dng", metadata=param.metadata):
 
 def capture_jpeg(output_filename="cap_jpg.jpg", metadata=param.metadata):
     picam2 = Picamera2()
-    
+
     exposure = metadata["ExposureTime"]  # Read from parameters module
     gain = metadata["AnalogueGain"] * metadata["DigitalGain"]  # Read from parameters module
 
@@ -64,5 +64,4 @@ def capture_jpeg(output_filename="cap_jpg.jpg", metadata=param.metadata):
 
 
 if __name__ == "__main__":
-    capture_dng()
     capture_dng_jpeg()
